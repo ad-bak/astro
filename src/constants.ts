49d40ea9fc586ce5c1dd622df80d494940e0c424
Adnan <adbak.dev@gmail.com>
2024-08-03 19:10:28 +0200

    limit

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "TechPeople Blog";
 export const SITE_DESCRIPTION = "Articles, Stories & Tutorials from TechPeople";
-export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 6;
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
+export const ARTICLES_PER_PAGE = 3;
